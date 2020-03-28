dbb8f2e4c8efae0090b559ee38b01c353b291372
Terry-zh <496971418@qq.com>
2020-03-28 20:21:39 +0800

    [fix] MathJax

--- .umirc.js
+++ .umirc.js
@@ -21,10 +21,29 @@ export default {
       },
     ],
   ],
-  scripts: ['https://s9.cnzz.com/z_stat.php?id=1278687945&web_id=1278687945',
-{
-  src: "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js",
-  async:""
-}],
+  headScripts: [
+    `
+    window.MathJax = {
+      tex2jax: {
+        inlineMath: [["$","$"], ["\\(", "\\)"]],],
+        displayMath: [["$$", "$$"], ["\\[", "\\]"]],},
+      skipTags: ["script", "noscript", "style", "textarea", "pre", "code", "a"]
+      showProcessingMessages: false,
+      messageStyle: 'none'
+    };
+  `,
+    {
+    src: "https://cdn.jsdelivr.net/npm/mathjax@2.7.5/MathJax.js?config=TeX-MML-AM_CHTML",
+    integrity: "sha256-nvJJv9wWKEm88qvoQl9ekL2J+k/RWIsaSScxxlsrv8k=",
+    crossorigin: "anonymous"
+    },
+  ],
+  scripts: [
+    'https://s9.cnzz.com/z_stat.php?id=1278687945&web_id=1278687945',
+    {
+      src: "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js",
+      async:""
+    }
+  ],
   styles: ['a[title=站长统计] { display: none; }'],
 };
