82956a69cc507707b3bc1236b619d5c5a15fda1f
Terry-zh <496971418@qq.com>
2020-04-21 17:10:03 +0800

    [CI]last

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,23 +2,13 @@
 # deploy
 
 # pwd
-# ls -lrt
-# eval $(ssh-agent)
-# git config --global user.name "terryzh"
-# git config --global user.email "496971418@qq.com"
-# git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
-# cp -r public/* alg_Blog
-# cd alg_Blog
-# git add . && git commit -m "[CI] auto commit" && git push
-
-cd ~/.ssh/
 ls -lrt
-cat config
-
-ssh -i ~/.ssh/id_rsa_8d0e24dbda940f511d7310dbfcee77ab root@39.104.123.222
-cd /opt/dumi
-ls -lrt
-# git pull
-
+eval $(ssh-agent)
+git config --global user.name "terryzh"
+git config --global user.email "496971418@qq.com"
+git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
+cp -r public/* alg_Blog
+cd alg_Blog
+git add . && git commit -m "[CI] auto commit" && git push
 
 exit 0
