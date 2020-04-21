edd632ef6e6babdc4fbf33987d75ae56aad7fbe7
Terry-zh <496971418@qq.com>
2020-04-21 09:59:32 +0800

    [ci]13

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # deploy
-openssl enc -d -aes-256-cbc -in .circleci/secrets.tar.enc -out ~/.ssh/secrets.tar -pass env:${pws}
-tar xvf ~/.ssh/secrets.tar -C ~/.ssh
-chmod 600 ~/.ssh/my_mac.pem
-chmod 600 ~/.ssh/id_rsa
-pwd
-eval $(ssh-agent)
-ssh-add ~/.ssh/id_rsa
-git config --global user.name "terryzh"
-git config --global user.email "496971418@qq.com"
-git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
-cp -r public/* alg_Blog
-cd alg_Blog
-git add . && git commit -m "[CI] auto commit" && git push
+echo $pws >> pwd.txt
+cat pwd.txt
+# openssl enc -d -aes-256-cbc -in .circleci/secrets.tar.enc -out ~/.ssh/secrets.tar -pass env:${pws}
+# tar xvf ~/.ssh/secrets.tar -C ~/.ssh
+# chmod 600 ~/.ssh/my_mac.pem
+# chmod 600 ~/.ssh/id_rsa
+# pwd
+# eval $(ssh-agent)
+# ssh-add ~/.ssh/id_rsa
+# git config --global user.name "terryzh"
+# git config --global user.email "496971418@qq.com"
+# git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
+# cp -r public/* alg_Blog
+# cd alg_Blog
+# git add . && git commit -m "[CI] auto commit" && git push
 
 
-ssh -i ~/.ssh/my_mac.pem  root@39.104.123.222:/opt/dumi
-rm -rf *
-git clone git@github.com:Terry-bear/alg_Blog.git -b master .
+# ssh -i ~/.ssh/my_mac.pem  root@39.104.123.222:/opt/dumi
+# rm -rf *
+# git clone git@github.com:Terry-bear/alg_Blog.git -b master .
