e21e5f4ef0e5ed75a198878c83408fd65e5c4240
Terry-zh <496971418@qq.com>
2020-04-21 10:22:32 +0800

    [ci]16

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,14 +3,16 @@
 
 pwd
 ls -lrt
+eval $(ssh-agent)
+git config --global user.name "terryzh"
+git config --global user.email "496971418@qq.com"
 git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
 cp -r public/* alg_Blog
 cd alg_Blog
 git add . && git commit -m "[CI] auto commit" && git push
 
-
-ssh  root@39.104.123.222:/opt/dumi
-pwd
+cd ~/.ssh/
 ls -lrt
+# ssh -i ~/.ssh/my_mac.pem  root@39.104.123.222:/opt/dumi
 # rm -rf *
 # git clone git@github.com:Terry-bear/alg_Blog.git -b master .
