0d96dfb2860487e66faa68f024e074a6f7fe3998
Terry-zh <496971418@qq.com>
2020-04-21 11:24:27 +0800

    [ci] 30

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # deploy
 
-pwd
-ls -lrt
-eval $(ssh-agent)
-git config --global user.name "terryzh"
-git config --global user.email "496971418@qq.com"
-git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
-cp -r public/* alg_Blog
-cd alg_Blog
-git add . && git commit -m "[CI] auto commit" && git push
+# pwd
+# ls -lrt
+# eval $(ssh-agent)
+# git config --global user.name "terryzh"
+# git config --global user.email "496971418@qq.com"
+# git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
+# cp -r public/* alg_Blog
+# cd alg_Blog
+# git add . && git commit -m "[CI] auto commit" && git push
 
-ssh -i ~/.ssh/id_rsa_03ce13edc73a1b24c09b1d0bdb63861e  root@39.104.123.222:/opt/dumi
+cd ~/.ssh/
 ls -lrt
-git pull
+
+ssh root@39.104.123.222:/opt/dumi
+ls -lrt
+# git pull
+
 
 exit 0
