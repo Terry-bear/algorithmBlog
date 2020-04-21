eb78ce7092896c23714829d54b7d7c15c6171692
Terry-zh <496971418@qq.com>
2020-04-21 10:18:09 +0800

    [ci]15

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,12 +3,14 @@
 
 pwd
 ls -lrt
-# git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
-# cp -r public/* alg_Blog
-# cd alg_Blog
-# git add . && git commit -m "[CI] auto commit" && git push
+git clone git@github.com:Terry-bear/alg_Blog.git -b master alg_Blog
+cp -r public/* alg_Blog
+cd alg_Blog
+git add . && git commit -m "[CI] auto commit" && git push
 
 
-# ssh -i ~/.ssh/my_mac.pem  root@39.104.123.222:/opt/dumi
+ssh  root@39.104.123.222:/opt/dumi
+pwd
+ls -lrt
 # rm -rf *
 # git clone git@github.com:Terry-bear/alg_Blog.git -b master .
